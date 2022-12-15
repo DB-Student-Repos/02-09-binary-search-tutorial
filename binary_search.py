391614661c3e05ef4000dc614d2a5df8a5d7f1f4
Shreeja Shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-12-15 22:21:46 +0100

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,21 @@
 def find(search_list, value):
-    pass
+    len_list = len(search_list)
+    midpoint = int(len_list / 2)
+    
+    if fo
+    while (len_list > 0):
+        if (search_list[0] > value) or (search_list[len_list - 1] < value):
+            raise ValueError("value not in array")
+
+        if (search_list[midpoint] > value):
+            len_list = midpoint
+            midpoint = int(len_list / 2)
+        else (search_list[midpoint] < value):
+            len_list = len_list - midpoint
+            midpoint = int(len_list / 2)
+        else:
+            return midpoint
+        
+        
+        
+   
