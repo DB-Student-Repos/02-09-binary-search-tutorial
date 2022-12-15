2dfcf3ff77b92119fa8970f36c3c9b020a214164
Shreeja Shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-12-15 22:23:21 +0100

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -2,7 +2,6 @@ def find(search_list, value):
     len_list = len(search_list)
     midpoint = int(len_list / 2)
     
-    if fo
     while (len_list > 0):
         if (search_list[0] > value) or (search_list[len_list - 1] < value):
             raise ValueError("value not in array")
