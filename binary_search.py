96f7a97bcd45d99ebfc5056277d42a84d50118aa
Shreeja Shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-12-15 22:48:17 +0100

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -2,16 +2,16 @@ def find(search_list, value):
     len_list = len(search_list)
     midpoint = int(len_list / 2)
     beg = 0
-    end = -1
+    end = len_list-1
     
-    while (len_list > 0):
+    while (len_list >= 0):
         if (search_list[beg] > value) or (search_list[end] < value):
             raise ValueError("value not in array")
 
         if (search_list[midpoint] > value):
             end = midpoint
         elif (search_list[midpoint] < value):
-            beg = midpoint
+            beg = midpoint + 1
         else:
             return midpoint
 
